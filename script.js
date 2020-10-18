4297bc3c660f2379ba75d8a485abdfe06dea233f
nicolascarrascocares <nicolascarrasco15@gmail.com>
2020-10-18 14:10:15 -0300

    quinto commit: carga de archivo js

---
+++ script.js
@@ -0,0 +1,11 @@
+//Variables
+let a = document.getElementById('a');
+let b = document.getElementById('b')
+let resultado = document.getElementById('resultado');
+
+function sumar(a,b){
+    let suma = +a.value + +b.value;
+    resultado.style.display = 'block';
+    let cadenaTexto = resultado.innerHTML = 'El resultado es: ' + suma;
+    return cadenaTexto;
+}
